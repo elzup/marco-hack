52be78fe7090cd12237df936a00fec39cfb9ef5a
elzzup <guild0105@gmail.com>
2015-06-02 23:12:52 +0900

    refactor py code

--- scrape.py
+++ scrape.py
@@ -2,27 +2,26 @@
 from selenium import webdriver
 import datetime
 import time
-import sys
+# import sys
 import config as cfg
+
 
 def inputText(driver, s, v):
     driver.find_element_by_css_selector(s).send_keys(v)
 
+
 def click(driver, s):
     driver.find_element_by_css_selector(s).click()
+
 
 def clickValue(driver, s):
     click(driver, "[value*='" + s + "']")
     # click(driver, "[value*='" + s + "']")
 
-def select(driver, name, value):
-    driver.find_element_by_css_selector("select[name=" + name + "]>option[value='" + value + "']").click()
 
-# try:
-#     (tmp, username, password) = (sys.argv)
-# except ValueError:
-#     print("usage: $ python scrape.py [username] [password]")
-#     exit()
+def select(driver, name, value):
+    driver.find_element_by_css_selector(
+            "select[name=" + name + "]>option[value='" + value + "']").click()
 
 driver = webdriver.Firefox()
 url = 'https://marco.ms.dendai.ac.jp/PTDU79130R/AX0101.aspx?mode=timeout'
@@ -33,7 +32,10 @@ driver.get(url)
 inputText(driver, "#TextBox_UserID", cfg.user['name'])
 inputText(driver, "#TextBox_Password", cfg.user['passwd'] + "\n")
 
-driver.execute_script('javascript:location.href="/PTDU79130R/AX1301.aspx?targeturl=https://marco.ms.dendai.ac.jp/ReportServer/Pages/ReportViewer.aspx?%2fPTDU79130R%2freport_GSY0205&params=USER_ID&rs:Command=Render&system=rs";')
+js_script = 'javascript:location.href="/PTDU79130R/AX1301.aspx?targeturl=\
+https://marco.ms.dendai.ac.jp/ReportServer/Pages/ReportViewer.aspx?%2fPTD\
+U79130R%2freport_GSY0205&params=USER_ID&rs:Command=Render&system=rs";'
+driver.execute_script(js_script)
 
 target_day = cfg.START_DAY
 
@@ -41,12 +43,14 @@ datas = []
 datas.append('')
 for i in range(cfg.REPEAT):
     datestr = target_day.strftime("%Y/%m/%d")
-    driver.execute_script('javascript:document.getElementById("ReportViewerControl_ctl04_ctl05_txtValue").value="' + datestr + '"')
+    driver.execute_script('javascript:document.getElementById("ReportViewerCont\
+rol_ctl04_ctl05_txtValue").value="' + datestr + '"')
     click(driver, "#ReportViewerControl_ctl04_ctl00")
     time.sleep(4)
     while True:
         try:
-            trs = driver.find_elements_by_css_selector("table table table table [valign=top]")
+            selector = "table table table table [valign=top]"
+            trs = driver.find_elements_by_css_selector(selector)
             break
         except Exception:
             time.sleep(1)
@@ -63,7 +67,6 @@ for i in range(cfg.REPEAT):
         except Exception:
             pass
     target_day = target_day + datetime.timedelta(1)
-# 
 
 
 f = open("./out.csv", "a")
